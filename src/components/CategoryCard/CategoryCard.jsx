9c2b4929728226415c672d6ea99fe9c80964037f
Ömer Türkay <omerturkay@192.168.1.105>
2025-08-14 06:42:07 +0300

    sadggsad

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./CategoryCard.scss";
 import FadeInSection from "../FadeInSection/FadeInSection";
 
 const CategoryCard = ({ img, categoryName, linkName }) => {
   return (
-    <Link to={`/kategoriler/${linkName}`} className="categorycard">
-      <FadeInSection>
+    <FadeInSection>
+      <Link to={`/kategoriler/${linkName}`} className="categorycard">
         <div className="categoryContent">
-          <h3>{categoryName}</h3>
           <div className="backgroundImg">
             <img src={img} alt={categoryName} />
           </div>
+          <h3>{categoryName}</h3>
         </div>
-      </FadeInSection>
-    </Link>
+      </Link>
+    </FadeInSection>
   );
+};
+
+CategoryCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  categoryName: PropTypes.string.isRequired,
+  linkName: PropTypes.string.isRequired,
 };
 
 export default CategoryCard;
